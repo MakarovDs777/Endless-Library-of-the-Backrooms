887df91fc2fb785dee6128440d06a3415e10d982
MakarovDs777 <42496353+MakarovDs777@users.noreply.github.com>
2024-01-28 12:02:25 +0300

    Update Eng.Version.BackroomsLibrary.py

--- Eng.Version.BackroomsLibrary.py
+++ Eng.Version.BackroomsLibrary.py
@@ -1,48 +1,48 @@
 import itertools
 
 def get_book_text(stellage_number, book_number):
-    # Получаем символы, из которых состоит книга
+    # We get the symbols that make up the book
     characters = get_characters(stellage_number, book_number)
     
-    # Преобразуем символы в текст
+    # Converting characters to text
     text = ''.join(characters)
     
     return text
 
 def get_characters(stellage_number, book_number):
-    # Получаем все возможные символы для данного стеллажа
+    # We get all possible symbols for this rack
     all_characters = get_all_characters(stellage_number)
     
-    # Получаем символы, из которых состоит книга
+    # We get the symbols that make up the book
     book_characters = get_book_characters(all_characters, book_number)
     
     return book_characters
 
 def get_all_characters(stellage_number):
-    # Получаем все возможные символы для данного стеллажа
-    # Например, для стеллажа номер 1 это будут все буквы алфавита, знаки препинания и точки
-    # Вам нужно определить, какие символы включать в каждый стеллаж
-    # и реализовать соответствующую логику здесь
-    
-    # Вернем все возможные символы, состоящие из стольких букв, сколько равно номеру стеллажа
+    # We get all possible symbols for this rack
+    # For example, for rack number 1, it will be all the letters of the alphabet, punctuation marks and dots
+    # You need to determine which symbols to include in each rack
+    # and implement the appropriate logic here
+
+    # We will return all possible characters consisting of as many letters as are equal to the number of the rack
     return list(itertools.product('abcdefghijklmnopqrstuvwxyz', repeat=stellage_number))
 
 def get_book_characters(all_characters, book_number):
-    # Получаем символы, из которых состоит книга
-    # Например, если в стеллаже есть 100 символов, а номер книги равен 1,
-    # то первая книга будет состоять из первого символа в списке all_characters
-    
-    # Проверяем, что номер книги не превышает количество символов в стеллаже
+    # We get the characters that make up the book
+    # For example, if there are 100 characters in the shelf and the book number is 1,
+    # then the first book will consist of the first character in the all_characters list
+
+    # Check that the book number does not exceed the number of characters in the rack
     if book_number > len(all_characters):
         raise ValueError('Неверный номер книги')
     
-    # Получаем символы, из которых состоит книга
+    # We get the characters that make up the book
     book_characters = list(all_characters[book_number - 1])
     
     return book_characters
 
-# Пример использования
-stellage_number = 2
-book_number = 1
+# Example of usage
+stellage_number = int(input("Enter the rack number: "))
+book_number = int(input("Enter the book number: "))
 book_text = get_book_text(stellage_number, book_number)
-print(book_text)
+print("Book: ",book_text,sep="\n")
